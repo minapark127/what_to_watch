184626ff4bf514dc3ac15299a4de78dcb05243c0
Mina <mina.park127@gmail.com>
2020-07-09 21:57:16 +1000

    counter with class components and state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  plus = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+    // this.setState({ count: this.state.count + 1 });
+    // both the same
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>Try plus or minus!</h1>
+        <h2>Number: {this.state.count}</h2>
+        <button onClick={this.plus}>plus</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
