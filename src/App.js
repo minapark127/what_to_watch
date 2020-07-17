1566967e6c0fc7774424e1f2df494ef1e4350048
Mina <mina.park127@gmail.com>
2020-07-17 23:08:34 +1000

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 import React from "react";
 
 class App extends React.Component {
-  state = {
-    count: 0,
-  };
-  plus = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-    // this.setState({ count: this.state.count + 1 });
-    // both the same
-  };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
-  render() {
-    return (
-      <div>
-        <h1>Try plus or minus!</h1>
-        <h2>Number: {this.state.count}</h2>
-        <button onClick={this.plus}>plus</button>
-        <button onClick={this.minus}>minus</button>
-      </div>
-    );
-  }
+  render() {}
 }
 
 export default App;
