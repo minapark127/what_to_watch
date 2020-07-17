864490fd838148cbde5c4bc54140406b53636d5d
Mina <mina.park127@gmail.com>
2020-07-17 23:39:05 +1000

    fetching movie list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
-  render() {}
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovieList = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({ movies, isLoading: false });
+    console.log(this.state.movies);
+  };
+
+  componentDidMount() {
+    this.getMovieList();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
 }
 
 export default App;
