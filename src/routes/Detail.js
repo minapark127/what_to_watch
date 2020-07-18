3875adcf31778a2bbc184b55052e70ec380a19e3
Mina <mina.park127@gmail.com>
2020-07-18 22:02:06 +1000

    redirecting on Detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-function Detail(props) {
-  console.log(props.history.location.state);
-  const title = props.history.location.state.title;
-  return <div>{title}</div>;
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    const { location } = this.props;
+    if (!location.state) {
+      return null;
+    } else {
+      return <div>{<h1>{location.state.title}</h1>}</div>;
+    }
+  }
 }
 
 export default Detail;
