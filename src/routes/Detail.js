09504c63e5d8474d6eae1ee54bda60b4a0261018
Mina <mina.park127@gmail.com>
2020-07-23 22:32:57 +1000

    styling movie detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Detail.scss";
 
 class Detail extends React.Component {
   componentDidMount() {
