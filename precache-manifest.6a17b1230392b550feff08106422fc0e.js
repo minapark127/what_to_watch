7af932c0ee4ab1c4f8694e91f1d965fe638b25ad
Mina <mina.park127@gmail.com>
2020-07-24 17:20:00 +1000

    Updates

---
+++ precache-manifest.6a17b1230392b550feff08106422fc0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b84c4aca213110e794f75881c0c6264",
+    "url": "/what_to_watch/index.html"
+  },
+  {
+    "revision": "40487220e9e7245505e6",
+    "url": "/what_to_watch/static/css/main.654cace4.chunk.css"
+  },
+  {
+    "revision": "bd20640b10886282c3ec",
+    "url": "/what_to_watch/static/js/2.098cd7ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/what_to_watch/static/js/2.098cd7ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40487220e9e7245505e6",
+    "url": "/what_to_watch/static/js/main.4d402ebe.chunk.js"
+  },
+  {
+    "revision": "012a23250cba326c304f",
+    "url": "/what_to_watch/static/js/runtime-main.d87320a0.js"
+  }
+]);
